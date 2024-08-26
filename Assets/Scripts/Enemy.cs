fbe0c3d075c400d941abbd26b6adc99ab8b0669b
Juliette Bénon <juliettebenon@gmail.com>
2024-08-26 22:50:12 +0200

    Use scriptable objects to define enemies, colliders and rigidbody and events for damages

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -9,28 +10,29 @@ public class Enemy : MonoBehaviour
     [SerializeField] private EnemySO enemySO;
     private float timerDamageWithHero;
     private float lifePoints;
+    public static event EventHandler<OnAttackHeroEventArgs> OnAttackHero;
+    public class OnAttackHeroEventArgs: EventArgs{
+        public float attackStat;
+    }
+    // Timers
+    private float timerAutomaticHeroAttack;
+    private float maxTimerAutomaticHeroAttack = 0.5f;
 
+
+    private void Awake(){
+        timerAutomaticHeroAttack = maxTimerAutomaticHeroAttack;
+    }
     private void Start(){
         timerDamageWithHero = enemySO.maxTimerDamageWithHero;
         lifePoints = enemySO.lifePoints;
+        Hero.Instance.OnAttackEnemy += Enemy_OnAttackEnemy;
     }
 
     private void Update()
     {
+        timerAutomaticHeroAttack += Time.deltaTime;
         // Move closer to the hero
         Move();
-
-        // Handle damages
-        if (HasCollisionWithHero())
-        {
-            if (timerDamageWithHero >= enemySO.maxTimerDamageWithHero){
-                lifePoints -= Hero.Instance.GetAttackStat() / enemySO.defenseStat;
-                Hero.Instance.ChangeLifePoints(- enemySO.attackStat / Hero.Instance.GetDefenseStat());
-                timerDamageWithHero = 0f;
-            }
-            timerDamageWithHero += Time.deltaTime;
-        }
-
         // Handle death
         if (lifePoints <= 0){
             Destroy(gameObject);
@@ -47,10 +49,26 @@ public class Enemy : MonoBehaviour
         transform.up = Vector3.Slerp(transform.up, moveDir, enemySO.rotateSpeed * Time.deltaTime);
     }
 
-    private bool HasCollisionWithHero()
-    {
-        float distanceToHero = Vector3.Distance(Hero.Instance.transform.position, transform.position);
-        return distanceToHero - enemySO.collisionDiameter - Hero.Instance.GetCollisionDiameter() < 0;
+    private void UpdateLifePoints(float lifePointsUpdate){
+        lifePoints = lifePoints + lifePointsUpdate;
     }
+
+    private void Enemy_OnAttackEnemy(object sender, Hero.OnAttackEnemyEventArgs e){
+        if (this == e.enemy){
+            UpdateLifePoints(- e.attackStat / enemySO.defenseStat);
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (timerAutomaticHeroAttack >= maxTimerAutomaticHeroAttack){
+            timerAutomaticHeroAttack = 0f;
+            // Enemy damages hero
+            OnAttackHero?.Invoke(this, new OnAttackHeroEventArgs{
+                attackStat = enemySO.attackStat});
+
+        }
+    }
+
 
 }
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -12,49 +13,77 @@ public class Hero : MonoBehaviour
     private GameInputs gameInputs;
     private float moveSpeed = 4f;
     private float rotateSpeed = 8f;
-    private float collisionDiameter = 1f;
+    private Rigidbody2D playerRigidbody2d;
+    public event EventHandler<OnAttackEnemyEventArgs> OnAttackEnemy;
+    public class OnAttackEnemyEventArgs: EventArgs{
+        public Enemy enemy;
+        public float attackStat;
+    }
     // Stats
     private float lifePoints = 100f;
     private float attackStat = 200f;
-    private float defenseStat = 100f;
+    private float defenseStat = 50f;
+    // Timers
+    private float timerAutomaticEnemyAttack;
+    private float maxTimerAutomaticEnemyAttack = 0.5f;
 
     private void Awake()
     {
         Instance = this;
+        playerRigidbody2d = GetComponent<Rigidbody2D>();
+        timerAutomaticEnemyAttack = maxTimerAutomaticEnemyAttack;
     }
 
+    private void Start(){
+        Enemy.OnAttackHero+= Hero_OnAttackHero;
+    }
     private void Update()
     {
-        Move();
-        
+        timerAutomaticEnemyAttack += Time.deltaTime;
         // Handle death
         if (lifePoints <= 0){
             Debug.Log("Game over");
         }
+        Debug.Log(lifePoints);
+    }
+
+    private void FixedUpdate() {
+        Move();
     }
 
     private void Move()
     {
-        // Move
         Vector2 inputVector = gameInputs.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, inputVector.y, 0f);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-        // Rotate
-        transform.up = Vector3.Slerp(transform.up, moveDir, rotateSpeed * Time.deltaTime);
+        
+        if (inputVector.x !=0 | inputVector.y != 0){
+            // Move
+            Vector3 moveDir = new Vector3(inputVector.x, inputVector.y, 0f);
+            playerRigidbody2d.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+            // Rotate
+            float moveAngle = -90 + Mathf.Atan2(inputVector.y, inputVector.x) * Mathf.Rad2Deg;
+            Quaternion moveQuaternion = Quaternion.AngleAxis(moveAngle, Vector3.forward);
+            playerRigidbody2d.MoveRotation(Quaternion.Slerp(transform.rotation, moveQuaternion, Time.deltaTime * rotateSpeed));
+        }
     }
 
-    // Functions to set hero parameters
-    public void ChangeLifePoints(float lifePointsChange){
-        lifePoints += lifePointsChange;
+    private void UpdateLifePoints(float lifePointsUpdate){
+        lifePoints = lifePoints + lifePointsUpdate;
     }
-    // Functions to access hero parameters
-    public float GetCollisionDiameter(){
-        return collisionDiameter;
+
+    private void Hero_OnAttackHero(object sender, Enemy.OnAttackHeroEventArgs e){
+        UpdateLifePoints(- e.attackStat / defenseStat);
     }
-    public float GetAttackStat(){
-        return attackStat;
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (timerAutomaticEnemyAttack >= maxTimerAutomaticEnemyAttack){
+            timerAutomaticEnemyAttack = 0f;
+            // Hero damages enemy
+            OnAttackEnemy?.Invoke(this, new OnAttackEnemyEventArgs{
+                enemy = collider.gameObject.GetComponent<Enemy>(),
+                attackStat = attackStat});
+
+        }
     }
-    public float GetDefenseStat(){
-        return defenseStat;
-    }
+
 }
